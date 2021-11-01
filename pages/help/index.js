81db6a1e982581df8f6330295e36ff22adbd2cfe
Lintang Aji Yoga Pratama <lintangajiyogapratama@gmail.com>
2021-11-01 13:07:40 +0700

    Revert "tambahan ui help dan tambahan config di tailwind"

--- pages/help/index.js
+++ pages/help/index.js
@@ -5,66 +5,19 @@ export default function Template() {
   return (
     <div className="flex flex-col">
       <Head>
-        <title>PetSpace - Status Kucing Titipan</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"></link>
+        <title>PetSpace - Cariin Tempat Penitipan Hewan Buat Kamu</title>
+        <link rel="icon" href="/petspace.png" />
       </Head>
-      <body className="font-body">
+      <body className="">
         <div className="h-full bg-gray-100">
           <div className="block box-border bg-white max-w-md w-full mx-auto h-full">
             <div className="flex flex-col h-screen">
               <div className="mx-5 my-10">
-                  {/* Mulai ngoding di sini */} 
-                    {/* Title */}
-                    <div className="text-3xl font-semibold tracking-tight ">Bantuan</div>
-                    {/* Description */}
-                    <p className="tracking-tight text-md text-gray-500 mt-3">Kamu butuh bantuan? Lihatlah beberapa daftar pertanyaan berikut</p>
-                    {/* Box  */}
-                    <div className="group box w-100% bg-abu h-lg border-abu border-2 rounded-lg mt-8 hover:bg-blue">
-                        <div className="content">
-                            <p className="tracking-tight text-md text-form-600 my-5 mx-5 leading-3 group-hover:text-white">Tentang Pet Space</p>
-                        </div>
-                    </div>
-                    {/* Box 2 */}
-                    <div className="group box w-100% bg-abu h-lg border-abu border-2 rounded-lg mt-4 hover:bg-blue">
-                        <div className="content">
-                            <p className="tracking-tight text-md text-form-600 my-5 mx-5 leading-3 group-hover:text-white">Mengapa kami membangun PetSpace?</p>
-                        </div>
-                    </div>
-                    {/* Box 3 */}
-                    <div className="group box w-100% bg-abu h-lg border-abu border-2 rounded-lg mt-4 hover:bg-blue">
-                        <div className="content">
-                            <p className="tracking-tight text-md text-form-600 my-5 mx-5 leading-3 group-hover:text-white">Tentang Pet Space</p>
-                        </div>
-                    </div>
-                    {/* Box 4 */}
-                    <div className="group box w-100% bg-abu h-lg border-abu border-2 rounded-lg mt-4 hover:bg-blue">
-                        <div className="content">
-                            <p className="tracking-tight text-md text-form-600 my-5 mx-5 leading-3 group-hover:text-white">Produk & Layanan</p>
-                        </div>
-                    </div>
-                    {/* Box 5 */}
-                    <div className="group box w-100% bg-abu h-lg border-abu border-2 rounded-lg mt-4 hover:bg-blue">
-                        <div className="content">
-                            <p className="tracking-tight text-md text-form-600 my-5 mx-5 leading-3 group-hover:text-white">Prosedur penitipan hewan di Pet Space </p>
-                        </div>
-                    </div>
-                    {/* Box 6 */}
-                    <div className="group box w-100% bg-abu h-lg border-abu border-2 rounded-lg mt-4 hover:bg-blue">
-                        <div className="content">
-                            <p className="tracking-tight text-md text-form-600 my-5 mx-5 leading-3 group-hover:text-white">Butuh bantuan ?</p>
-                        </div>
-                    </div>
-                    {/* Box 7 */}
-                    <div className="group box w-100% bg-abu h-lg border-abu border-2 rounded-md mt-4 hover:bg-blue">
-                        <div className="content">
-                            <p className="tracking-tight text-md text-form-600 my-5 mx-5 leading-3 group-hover:text-white">Punya pertanyaan lain ?</p>
-                        </div>
-                    </div>
+                  {/* Mulai ngoding di sini */}
               </div>
             </div>
-            <Bar />
+
+            <Bar currentPage="help"/>
             <div className="w-full h-14"></div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,83 +1,46 @@
 module.exports = {
-  // font : "@import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');",
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    
     extend: {
       spacing: {
-        '100': "28rem",  
-      },
-      fontFamily: {
-        body : 'Inter', 
+        100: "28rem",
       },
       maxWidth: {
-        'bar': "168px",
+        bar: "168px",
       },
       minWidth: {
-        'bar': "80px",
+        bar: "80px",
       },
       colors: {
-        'bar': '#B9BCBE',
-        'cyan-main': '#63C6CD',
-        'cyan-secondary': '#47B0B8' ,
-        'search-bg': '#F5F5F5',
-        'search-font': '#88888C',
-      
+        bar: "#B9BCBE",
+        "blue-main": "#429FF2",
+        "blue-secondary": "rgba(66, 159, 242, 0.1)",
+        "yellow-pet": "#F4AB5C",
+        "search-bg": "#F5F5F5",
+        "search-font": "#88888C",
       },
       fontSize: {
-        'bar': '.75rem',
+        bar: ".75rem",
       },
       margin: {
-        '-0.5': '-2px'
+        "-0.5": "-2px",
       },
-      // Text Collor
-      textColor: theme => theme('colors'),
-      textColor: {
-        'primary': '#3490dc',
-        'secondary': '#ffed4a',
-        'text-form': '#423E3E;',
-        'white' :'#ffffff;',
+      backgroundImage: {
+        "card-status": "url('/status-card/card-bg.png')",
       },
-      // Border Collor
-      borderColor: theme => ({
-        ...theme('colors'),
-         DEFAULT: theme('colors.gray.300', 'currentColor'),
-        'primary': '#3490dc',
-        'secondary': '#ffed4a',
-        'abu': '#E0F0FD',
-       }),
-      // Background collor 
-      backgroundColor: theme => ({
-        ...theme('colors'),
-        'blue': '#429FF2',
-        'secondary': '#ffed4a',
-        'abu': '#FCFEFF;',
-       }),
-      // Form
-      // height form help
-      height: {
-        sm: '8px',
-        md: '52px',
-        lg: '60px',
-        xl: '48px',
-       },
-      //  Width form Help
-      width: {
-        '1/7': '14.2857143%',
-        '2/7': '28.5714286%',
-        '3/7': '42.8571429%',
-        '4/7': '57.1428571%',
-        '5/7': '71.4285714%',
-        '6/7': '85.7142857%',
-      }
+      fontFamily: {
+        main: ["Poppins"],
+        secondary: ["Inter"],
+      },
+    },
+    screens: {
+      md: "350px",
     },
   },
   variants: {
-    extend: {
-    
-    },
+    extend: {},
   },
   plugins: [],
 };
